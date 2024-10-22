01ce77b49933d50f1f9dd2a5ba172442ad421ac6
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-10-22 19:07:27 +0200

    added campers store

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import CatalogPage from "./pages/CatalogPage";
-import CamperDetailPage from "./pages/CamperDetailPage";
-import Header from "./components/Header";
+import HomePage from "./pages/HomePage/HomePage";
+import CatalogPage from "./pages/CatalogPage/CatalogPage";
+import CamperDetailPage from "./pages/CamperDetailPage/CamperDetailPage";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
---
+++ src/redux/campersSlice.js
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  campers: [],
+  status: "idle",
+  error: null,
+};
+
+export const fetchCampers = createAsyncThunk(
+  "campers/fetchCampers",
+  async () => {
+    const response = await axios.get(
+      "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers"
+    );
+    return response.data;
+  }
+);
+
+const campersSlice = createSlice({
+  name: "campers",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCampers.peding, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchCampers.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.campers = action.payload;
+      })
+      .addCase(fetchCampers.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default campersSlice.reducer;
