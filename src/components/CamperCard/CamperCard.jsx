4cdc9dafb26bee68a981eea706c880a6a68a9808
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-10-27 17:09:46 +0100

    done CamperCard

--- src/components/CamperCard/CamperCard.jsx
+++ src/components/CamperCard/CamperCard.jsx
@@ -5,6 +5,16 @@ import iconSprite from "../../assets/icons.svg";
 import styles from "./CamperCard.module.css";
 
 const CamperCard = ({ camper }) => {
+  const maxDescriptionLength = 60;
+  const maxNameLength = 22;
+
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.slice(0, maxLength) + "...";
+    }
+    return text;
+  };
+
   const featuresList = [
     { name: "AC", icon: "ac", available: camper.AC },
     {
@@ -12,9 +22,9 @@ const CamperCard = ({ camper }) => {
       icon: "transmission",
       available: camper.transmission === "automatic",
     },
-    { name: "Kitchen", icon: "kitchen", available: camper.kitchen },
+    { name: "Kitchen", icon: "cup_hot", available: camper.kitchen },
     { name: "TV", icon: "tv", available: camper.TV },
-    { name: "Bathroom", icon: "bathroom", available: camper.bathroom },
+    { name: "Bathroom", icon: "shower", available: camper.bathroom },
     { name: "Petrol", icon: "petrol", available: camper.engine === "petrol" },
   ];
 
@@ -25,7 +35,9 @@ const CamperCard = ({ camper }) => {
       </div>
       <div className={styles.details}>
         <div className={styles.name_and_price_favorite}>
-          <h2 className={styles.name}>{camper.name}</h2>
+          <h2 className={styles.name}>
+            {truncateText(camper.name, maxNameLength)}
+          </h2>
           <div className={styles.price_and_favorite}>
             <p className={styles.price}>
               €{camper.price ? camper.price.toFixed(2) : "N/A"}
@@ -38,7 +50,15 @@ const CamperCard = ({ camper }) => {
 
         <div className={styles.rating_and_location}>
           <span className={styles.rating}>
-            <svg className={styles.star_icon}>
+            <svg
+              className={styles.star_icon}
+              style={{
+                fill:
+                  camper.reviews && camper.reviews.length > 0
+                    ? "#ffc531"
+                    : "currentColor",
+              }}
+            >
               <use xlinkHref={`${iconSprite}#icon-rating_star`}></use>
             </svg>
             {camper.rating} ({camper.reviews ? camper.reviews.length : 0}{" "}
@@ -52,21 +72,22 @@ const CamperCard = ({ camper }) => {
           </span>
         </div>
 
-        <p className={styles.description}>{camper.description}</p>
+        <p className={styles.description}>
+          {truncateText(camper.description, maxDescriptionLength)}
+        </p>
 
         <div className={styles.features}>
           {featuresList
             .filter((feature) => feature.available)
             .map((feature, index) => (
               <div key={index} className={styles.feature}>
-                <svg className={styles.featureIcon}>
+                <svg className={styles.feature_icon}>
                   <use xlinkHref={`${iconSprite}#icon-${feature.icon}`}></use>
                 </svg>
                 {feature.name}
               </div>
             ))}
         </div>
-
         <Link
           to={`/catalog/${camper.id}`}
           className={`primary_button ${styles.show_more_button}`}
